487dcd73fbed534cda8e63ceaf97eb0961cb28e4
Leandro Durães Freitas <duraesleandro21@gmail.com>
2024-08-17 17:54:09 -0300

    3 - Supports for forum

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Support;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view("admin.support.index");
+    public function index(Support $support){
+        $supports = Support::all();
+
+        return view("admin.support.index", compact('supports'));
     }
 }
