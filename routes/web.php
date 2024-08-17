2071084a48be2e30352809f758f87759f94a1080
Leandro Durães Freitas <duraesleandro21@gmail.com>
2024-08-17 16:30:02 -0300

    2 - Mudanças nas rotas iniciais

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{SupportController};
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/", [SupportController::class, 'index'])->name("support.index");
